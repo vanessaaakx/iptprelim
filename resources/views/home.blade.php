e7c2ac43191d79bdd6302662c1b7e8fff28203b0
vanessaaakx <vanessahappiness14@gmail.com>
2022-09-17 16:45:18 +0800

    initial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prelim Project</title>
+    <script src="{{asset('js/bootsrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+<body>
+    <div class="text-center">
+        <nav>
+            <a href="{{url('/')}}">Home</a>
+            <a href="{{url('/about')}}">About Us</a>
+            <a href="{{url('/profile')}}">Profile</a>
+</nav>
+        <h1>Prelim Project - Page 1</h1>
+
+        <div class="card shadow-sm mx-auto" style="width: 500px">
+            <div class="card-body">
+                <div class="card-title">Welcome Message</div>
+                <p>
+                    This is just a test of the emergency broadcst system.
+</p>
+</body>
+</html>
